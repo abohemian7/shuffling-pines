b5d50c736e21434c6046da5cfa962d1874b7c970
Andrew Boyer <boyer.andrew.7@gmail.com>
2015-11-08 20:32:51 -0500

    working through it

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
-var app = angular.module('shuffling', []);
 
-app.factory('notify', ['$window', function(win) {
+angular.module('shuffling', [])
+
+    .factory('notify', ['$window', function(win) {
    var msgs = [];
    return function(msg) {
      msgs.push(msg);
@@ -9,39 +10,75 @@ app.factory('notify', ['$window', function(win) {
        msgs = [];
      }
    };
- }]);
+ }])
 
-app.controller('FormController', [function(){
+    .controller('FormController', ['initGuest', 'notify', function(initGuest, notify){
 
-  var vm = this;
+        var vm = this;
 
-    var onSubmit = function(){
-      tester="duh"
-      console.log(tester);
+        var guestStatus = vm.guestStatus;
+
+        this.onSubmit = function(name,transDate,status,location){
+
+            var tester="duh"
+
+            var Guest = {
+            name : name || 'Default name',
+            transitionDate : transDate || new Date(),
+            status : guestStatus || 'Drop-off',
+            location : location || 'nowhere'
+            };
+
+            //console.log($scope.guestStatus);
+            console.log(guestStatus);
+
+            console.log(tester + JSON.stringify(Guest));
+        };
+
+        this.transDate = new Date('yyyy-mm-dd');
+
+        // $scope.initGuests = ['test','test2'];
+        //$scope.notify = notify;
+
+        console.log(initGuest);
+
+}])
+
+    .value('initGuest','John Harvard');
+
+app.directive('guests', ['dateFilter','$interval',function(dateFilter, $interval){
+
+  var link = function(scope, element,attrs){
+
+  var format = element.attr('format') || 'HH:mm:ss';
+  var updateTime = function(){
+    var now = Date.now();
+
+    element.html(dateFilter(now, format));
+
     };
 
-    var Guest = function(name, transitionDate, status, location){
-        this.Name = name || 'Default name';
-        this.transitionDate = transitionDate || new Date();
-        this.status = status || 'Drop-off';
-        this.location = location;
+    $interval(updateTime,1000);
 
-        return this;
+    updateTime();
+
+  };
+
+  return {
+    restrict: ['E'],
+    link: link
+  };
+
+}])
+    .factory('guestFactory',['formFields', function($scope){
+
+    var Guest = {
+        name : $scope.name || 'Default name',
+        transitionDate : $scope.transitionDate || new Date(),
+        status : $scope.status || 'Drop-off',
+        location : $scope.location || 'nowhere'
     };
 
-    var initGuests = [new Guest('John Harvard',null,null,'front')];
-    console.log(initGuests);
-    console.log(JSON.stringify(initGuests));
-    localStorage.guests = JSON.stringify(initGuests);
-
-    var tester = "";
-
-    // delete - this.delete = true
-
-}]);
-
-app.controller('TabController', [function(){
-
-    var vm = this;
+    return {Guest};
 
 }]);
