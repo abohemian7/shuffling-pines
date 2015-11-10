0363e95bd5dcff36aa28a706d272337188d904fa
Andrew Boyer <boyer.andrew.7@gmail.com>
2015-11-09 23:57:50 -0500

    added a bunch of stuff, including soft deletes

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,9 @@
+Date.prototype.yyyymmdd = function() {
+    var yyyy = this.getFullYear().toString();
+    var mm = (this.getMonth()+1).toString(); // getMonth() is zero-based
+    var dd  = this.getDate().toString();
+    return yyyy +'-'+ (mm[1]?mm:"0"+mm[0]) +'-'+ (dd[1]?dd:"0"+dd[0]); // padding
+};
 
 angular.module('shuffling', [])
 
@@ -12,26 +18,31 @@ angular.module('shuffling', [])
         };
     }])
 
-    .controller('FormController', ['initGuest', 'notify', function(initGuest, notify){
+    .controller('FormController', ['initGuest', 'notify','localGuests', function(initGuest, notify, localGuests){
 
         var vm = this;
 
-        this.transDate = new Date('yyyy-mm-dd');
+        this.transDate = new Date().yyyymmdd;
+
+        this.localGuests = localGuests.data;
 
         this.onSubmit = function(name,transDate,status,location){
 
-            var tester="duh"
-
             var Guest = {
             name : name || 'Default name',
-            transitionDate : transDate || new Date(),
+            transitionDate : transDate.yyyymmdd() || new Date().yyyymmdd(),
             status : status || 'Drop-off',
             location : location || 'nowhere'
             };
 
-            console.log(tester + JSON.stringify(Guest));
+            console.log(JSON.stringify(Guest));
+            localGuests.addGuest(Guest);
 
         };
+
+        this.onDelete = function(guestIndex){
+            localGuests.removeGuest(guestIndex);
+        }
 
         console.log(initGuest);
 
@@ -41,45 +52,13 @@ angular.module('shuffling', [])
 
     .directive('clock', ['dateFilter','$interval',function(dateFilter, $interval){
 
-        var link = function(scope, element,attrs){
+        var link = function(scope, element){
 
             var format = element.attr('format') || 'HH:mm:ss';
             var updateTime = function(){
                 var now = Date.now();
 
-                element.html(dateFilter(now, format));
-
-            };
-
-            $interval(updateTime,1000);
-
-            updateTime();
-
-        };
-
-        return {
-            restrict: ['E'],
-            link: link
-        };
-
-    }])
-
-    .directive('guests', [function(){
-
-        var vm = this;
-
-        var baseHtml =
-
-
-        vm.html()
-
-        var link = function(scope, element,attrs){
-
-            var format = element.attr('format') || 'HH:mm:ss';
-            var updateTime = function(){
-                var now = Date.now();
-
-                element.html(dateFilter(now, format));
+                element.html(dateFilter(now, format) + " hrllo");
 
             };
 
@@ -111,5 +90,49 @@ angular.module('shuffling', [])
 
     // service = values by ref
     .service('localGuests',[function(){
+
+        var oldGuests = [];
+
+        var addGuest = function(aGuest){
+
+            oldGuests.push(aGuest)
+            localStorage.setItem('guestList',JSON.stringify(oldGuests));
+
+        };
+
+        try{
+
+            oldGuests = (JSON.parse(localStorage.getItem('guestList')) || []);
+
+        } catch(e){
+            console.log(e);
+        };
+
+        console.log(oldGuests.length);
+
+        if(!oldGuests.length){
+
+            var newGuest = {
+                name : 'Default name',
+                transitionDate : new Date().yyyymmdd(),
+                status : 'Drop-off',
+                location : 'nowhere'
+            };
+
+            addGuest(newGuest);
+        }
+
+        var softDelete = function(guestIndex){
+
+            oldGuests[guestIndex].delete = true;
+            localStorage.setItem('guestList',JSON.stringify(oldGuests));
+
+        };
+
+        return {
+            data: oldGuests,
+            addGuest: addGuest,
+            removeGuest: softDelete
+        };
 
     }]);
